f45f4fb992a2e60adbf340c0b6a14f25873a2d42
mafergus <matthew.n.ferguson@gmail.com>
2018-05-10 11:09:19 +0400

    Add colors

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { primary } from 'util/colors';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div style={{ position: "fixed", top: 0, left: 0, right: 0, bottom: "100%", backgroundColor: "red" }}>
       </div>
     );
   }
